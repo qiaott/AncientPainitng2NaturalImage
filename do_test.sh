b017db81a581ef933b024ca135e6f9b8c553feeb
qiaott <qiaott@zju.edu.cn>
2019-06-26 11:25:11 +0800

    edit

--- do_test.sh
+++ do_test.sh
@@ -1,13 +1,8 @@
-dataroot=.data/bird/
-model=mygan
-phase=test
-checkpoints_dir=.output/bird/split/checkpoints
-how_many=3000
-lambda_G=1.0
-lambda_F=1.0
-lambda_A=10.0
-lambda_B=10.0
-lambda_C=10.0
-lambda_D=10.0
-gpu_ids=1
-python test.py --dataroot $dataroot --name $name --model $model --phase $phase --no_dropout --checkpoints_dir $checkpoints_dir --lambda_G $lambda_G --lambda_F $lambda_F --lambda_A $lambda_A --lambda_B $lambda_B --lambda_C $lambda_C --lambda_D $lambda_D --gpu_ids $gpu_ids --how_many $how_many 
+dataroot=./data/bird/
+model=DSTN
+gpu_ids=0
+alpha_G=1.0
+alpha_F=0.5
+beta=10.0
+gamma=1.0
+python test.py --dataroot $dataroot --model $model --no_dropout --gpu_ids $gpu_ids --alpha_G $alpha_G --alpha_F $alpha_F --beta $beta --gamma $gamma
